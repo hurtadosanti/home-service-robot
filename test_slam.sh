63d8444e77a7a40336fed446f871bf3afc4c7a0e
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-03-18 09:14:21 +0000

    fix: fix issues with keyboard package

--- test_slam.sh
+++ test_slam.sh
@@ -5,4 +5,4 @@ xterm  -e  " source ../devel/setup.bash;roslaunch turtlebot_gazebo gmapping_demo
 sleep 5
 xterm  -e  " source ../devel/setup.bash;roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
 sleep 5
-xterm  -e  " source ../devel/setup.bash;roslaunch turtlebot_teleop keyboard_teleop.launch " & 
+xterm  -e  " source ../devel/setup.bash;roslaunch turtlebot_teleop keyboard_teleop.launch "
