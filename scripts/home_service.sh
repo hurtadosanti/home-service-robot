6e5d57129e209dbd02a3d3eb183612b3531d6f13
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-03-26 17:47:28 +0100

    test: review if source can be skipped

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-xterm  -e  " source ../devel/setup.bash;roslaunch turtlebot_gazebo turtlebot_world.launch" & 
+xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" & 
 sleep 5
-xterm  -e  " source ../devel/setup.bash;roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm  -e  "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm  -e  " source ../devel/setup.bash;roslaunch add_markers navigation.launch " & 
+xterm  -e  "roslaunch add_markers navigation.launch " & 
 sleep 5
-xterm  -e  " source ../devel/setup.bash;rosrun add_markers add_markers "&
+xterm  -e  "rosrun add_markers add_markers "&
 sleep 5
-xterm  -e  " source ../devel/setup.bash;rosrun pick_objects pick_objects "
+xterm  -e  "rosrun pick_objects pick_objects "
 
