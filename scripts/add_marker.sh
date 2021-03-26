1df7236e1e840acd821e123a21c6679ad17584d1
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-03-26 17:45:57 +0100

    chore: clean script

--- scripts/add_marker.sh
+++ scripts/add_marker.sh
@@ -5,4 +5,4 @@ xterm  -e  " source ../devel/setup.bash;roslaunch turtlebot_gazebo amcl_demo.lau
 sleep 5
 xterm  -e  " source ../devel/setup.bash;roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
 sleep 5
-xterm  -e  " source ../devel/setup.bash; rosrun add_markers add_markers; read -p Press_a_Key " 
+xterm  -e  " source ../devel/setup.bash;rosrun add_markers add_markers; read -p Press_a_Key " 
