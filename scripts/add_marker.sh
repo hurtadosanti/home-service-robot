b9d4299b8ce87f3668e49be7d735cc391f4bd7eb
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-03-28 14:42:12 +0200

    feat: include custom rviz

--- scripts/add_marker.sh
+++ scripts/add_marker.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 xterm  -e  "roslaunch add_markers world.launch" & 
 sleep 5
-xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
-sleep 5
 xterm  -e  "rosrun add_markers show_markers; read -p Press_a_Key " 
