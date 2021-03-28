dd829becfb8cde0b8700b7fccdb9b2284ef8a36a
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-03-28 14:52:43 +0200

    feat: use navitation packages

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" & 
-sleep 5
-xterm  -e  "roslaunch turtlebot_gazebo amcl_demo.launch" & 
-sleep 5
-xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
+xterm  -e  "roslaunch turtlebot_gazebo world.launch" & 
 sleep 5
 xterm  -e  "roslaunch turtlebot_teleop keyboard_teleop.launch "
