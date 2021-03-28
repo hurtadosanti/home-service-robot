e1d42a338fe8f91a2495dd5362e42d1ca3ef6faf
Santiago Hurtado <santiago.hurtado@gmail.com>
2021-03-28 12:34:41 +0200

    feat: prepare home service script

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" & 
-sleep 5
-xterm  -e  "roslaunch turtlebot_gazebo amcl_demo.launch" &
-sleep 5
-xterm  -e  "roslaunch add_markers home_service.launch " & 
+xterm  -e  "roslaunch add_markers home_service_navigation.launch" & 
 sleep 5
 xterm  -e  "rosrun add_markers add_markers "&
 sleep 5
